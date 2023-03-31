cf4e9d2cb5df007c315e558c66ae8e091422cb36
AdriaandeWit <adriaandewit3@gmail.com>
2023-03-31 10:04:47 +0200

    first commit

---
+++ src/test/java/nl/novi/Eindopdracht/EindopdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.Eindopdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
